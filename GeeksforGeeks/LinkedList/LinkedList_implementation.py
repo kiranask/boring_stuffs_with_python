74b4435f36d7996cd39318037fc3c60148b9c0a8
kkatte <kkatt@akamai.com>
2019-11-08 16:05:51 +0530

    New Codes

---
+++ GeeksforGeeks/LinkedList/LinkedList_implementation.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+    def prepend(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print('Previous Node is not present')
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+
+
+llist = LinkedList()
+llist.append('A')
+llist.append('B')
+llist.append('C')
+llist.append('D')
+llist.prepend('E')
+llist.insert_after_node(llist.head, 'X')
+llist.print_list()
