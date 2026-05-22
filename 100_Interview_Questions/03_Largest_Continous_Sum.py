e0bb98d757763540dcf590ab7e7f298061a10b14
Kiran Katte <kkatte@akamai.com>
2026-05-22 16:38:00 +0530

    updated

---
+++ 100_Interview_Questions/03_Largest_Continous_Sum.py
@@ -0,0 +1,11 @@
+list_sample = [1,2,3]
+list_sample.append(1)
+print(list_sample)
+list_sample.remove(2)
+print(list_sample)
+list_sample.insert(0,2)
+print(list_sample)
+print(list_sample.pop())
+
+
+# what is the differenec between pop and peek ?
